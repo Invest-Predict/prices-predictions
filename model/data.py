0f3cbaaab5363165b4d27701655d25507ca3836a
NastiaBur <s22d_burkova@179.ru>
2025-01-09 23:18:52 +0300

    Added new features and changed structure a bit
    OMG look at her...

--- model/data.py
+++ model/data.py
@@ -3,8 +3,8 @@ import polars as pl
 import datetime as dt
 import matplotlib.pyplot as plt
 import numpy as np
-from arch.unitroot import PhillipsPerron
-from statsmodels.tsa.stattools import adfuller
+# from arch.unitroot import PhillipsPerron
+# from statsmodels.tsa.stattools import adfuller
 
 # Здесь все признаки и все по датафрейму
 
@@ -27,12 +27,19 @@ class FinData():
         """
         self.df = pd.read_csv(df_path)
 
-        if column_names is not None:
-            self.df = self.df.rename(columns=column_names, errors='ignore')
+        self.df = self.df.rename(columns={'Yandex open' : 'open', 
+                                          'Yandex close' : 'close', 
+                                          'Yandex high' : 'high', 
+                                          'Yandex low' : 'low', 
+                                          'Yandex volume' : 'volume'}, 
+                                 errors='ignore')
         
         self.df.utc = pd.to_datetime(self.df.utc).dt.tz_localize(None)
         self.df.drop_duplicates(inplace=True)
         self.target : str
+
+        self.cat_features = []
+        self.numeric_features = ['open', 'close', 'high', 'low', 'volume']
 
         
     def make_binary_class_target(self):
@@ -144,12 +151,29 @@ class FinData():
             self.df[f'high_norms_{i}'] = self.df['high']/self.df['high'].shift(i)
             self.df[f'low_norms_{i}'] = self.df['low']/self.df['low'].shift(i)
 
+            if f'close_norms_{i}' not in self.numeric_features:
+                self.numeric_features += [f'close_norms_{i}', f'close_high_norms_{i}', f'high_norms_{i}', f'low_norms_{i}']
+
     def insert_time_features(self):
         # Добавляет минуты, дни, часы 
         # Убрать может раз вообще незначимо
+                                  
         self.df['hours'] = self.df['utc'].dt.hour
         self.df['day'] = self.df['utc'].dt.day_of_year
         self.df['minute'] = (self.df['utc'].dt.minute + 60 * self.df['hours'])
+
+        if 'hours' not in self.cat_features:
+            self.cat_features += ['hours', 'day', 'minute']
+    
+    def insert_holidays(self):
+    
+        self.df['is_holiday'] = self.df['utc'].dt.strftime('%m-%d').isin(['12-31', '01-01', '01-07', '02-14', '02-23', '03-08', '05-01']).astype('int')
+        self.df['pred_holiday'] = self.df['utc'].dt.strftime('%m-%d').isin(['11-11', '11-30', '12-30', '02-13', '02-22', '03-07', '04-30']).astype('int') # + black friday
+        self.df['week_pred_holiday'] = self.df['utc'].dt.strftime('%m-%d').isin(['12-24', '02-07', '02-16', '03-01', '04-24']).astype('int')
+
+        if 'is_holiday' not in self.cat_features:
+            self.cat_features += ['is_holiday', 'pred_holiday', 'week_pred_holiday']
+
 
     def insert_rolling_means(self, windows_ma = [3, 6, 18]):
         """
@@ -162,6 +186,9 @@ class FinData():
         for i in windows_ma:
             self.df[f'ma_{i}'] = self.df['close'].rolling(window = i, closed="left").mean()
             self.df[f'close_normed_ma_{i}'] = self.df['close']/self.df[f'ma_{i}']
+
+            if f'ma_{i}' not in self.numeric_features:
+                self.numeric_features += [f'ma_{i}', f'close_normed_ma_{i}']
             
     def insert_exp_rolling_means(self, windows_ema = [3, 6, 18]):
         """
@@ -174,52 +201,113 @@ class FinData():
         for i in windows_ema:
             self.df[f'ema_{i}'] = (self.df['close']).ewm(span=i).mean()
             self.df[f'close_normed_ema_{i}'] = self.df['close']/self.df[f'ema_{i}']
+
+            if f'ema_{i}' not in self.numeric_features:
+                self.numeric_features += [f'ema_{i}', f'close_normed_ema_{i}']
     
-    def insert_rsi(self, windows_rsa = [3, 6, 18]):
-        for i in windows_rsa:
+    def insert_rsi(self, windows_rsi = [3, 6, 18]):
+        for i in windows_rsi:
             self.df[f'rsi_{i}'] = self.df['close'] - self.df['close'].shift(i)
             self.df[f'close_normed_rsi_{i}'] = self.df['close']/self.df[f'rsi_{i}']
+
+            if f'rsi_{i}' not in self.numeric_features:
+                self.numeric_features += [f'rsi_{i}', f'close_normed_rsi_{i}']
+    
+    def insert_bollinger(self): # TODO add other windows
+        if 'ma_16' not in self.df.columns:
+            self.insert_rolling_means([16])
+        self.df['SD'] =np.sqrt(np.square((self.df["ma_16"] - self.df["close"])).rolling(16, closed='left').sum())
+        self.df['upper_bollinger'] = self.df["ma_16"] + self.df['SD'] * 2
+        self.df['low_bollinger'] = self.df["ma_18"] - self.df['SD'] * 2
+
+        self.df['close_normed_upper_bollinger'] = self.df['close']/self.df['upper_bollinger']
+        self.df['close_normed_low_bollinger'] = self.df['close']/self.df['low_bollinger']
+
+        if 'SD' not in self.numeric_features:
+                self.numeric_features += ['SD', 'low_bollinger', 'upper_bollinger', 'close_normed_upper_bollinger', 'close_normed_low_bollinger']
+    
+    def insert_high_low_diff(self, windows_hl = [3, 6, 18]):
+        for i in windows_hl:
+            self.df[f'hl_diff_{i}'] = self.df['high'].shift(i) - self.df['low'].shift(i)
+            self.df[f'close_normed_hl_diff_{i}'] = self.df['close']/self.df[f'hl_diff_{i}']
+
+            if f'hl_diff_{i}' not in self.numeric_features:
+                self.numeric_features += [f'hl_diff_{i}', f'close_normed_hl_diff_{i}']
+    
+    def insert_stochastic_oscillator(self, windows_st = [3, 6, 18]): # TODO add other windows
+
+        self.df['stochastic_oscillator'] = (self.df['close'] - self.df['low'].shift(18)) / (self.df['high'].shift(18) - self.df['low'].shift(18))
+        self.df['close_normed_stochastic_oscillator'] = self.df['close'] / self.df['stochastic_oscillator']
+        
+        if 'stochastic_oscillator' not in self.numeric_features:
+            self.numeric_features += ['stochastic_oscillator', 'close_normed_stochastic_oscillator']
+
+        for i in windows_st:
+            self.df[f'stochastic_oscillator_ma_{i}'] = self.df['stochastic_oscillator'].rolling(i).mean()
+            self.df[f'close_normed_stochastic_oscillator_ma_{i}'] = self.df['close'] / self.df[f'stochastic_oscillator_ma_{i}']
+
+            if f'stochastic_oscillator_ma_{i}' not in self.numeric_features:
+                self.numeric_features += [f'stochastic_oscillator_ma_{i}', f'close_normed_stochastic_oscillator_ma_{i}']
+    
+    def insert_random_prediction(self): # TODO think, if other normalization is needed
+        self.df['target_predict'] = np.exp(np.log1p(self.df['close'].shift(15)) + 4 * np.random.normal(1)) - self.df['close']
+        if 'target_predict' not in self.numeric_features:
+            self.numeric_features += ['target_predict']
+    
+    def insert_all(self, common_windows= None):
+        if common_windows is None:
+            common_windows = [3, 6, 18]
+        self.insert_shifts_norms(common_windows)
+        self.insert_time_features()
+        self.insert_holidays()
+        self.insert_rolling_means(common_windows)
+        self.insert_exp_rolling_means(common_windows)
+        self.insert_rsi(common_windows)
+        self.insert_bollinger()
+        self.insert_high_low_diff(common_windows)
+        self.insert_stochastic_oscillator(common_windows)
+        self.insert_random_prediction()
 
     def get_columns(self):
         return self.df.columns
 
 
-    def check_stationarity(self, columns):
-        """
-        Проверяет стационарность столбцов с использованием тестов Phillips-Perron (PP),
-        теста Дики-Фуллера (ADF).
+    # def check_stationarity(self, columns):
+    #     """
+    #     Проверяет стационарность столбцов с использованием тестов Phillips-Perron (PP),
+    #     теста Дики-Фуллера (ADF).
 
-        Параметры:
-            columns (list): Список названий столбцов для проверки на стационарность.
+    #     Параметры:
+    #         columns (list): Список названий столбцов для проверки на стационарность.
 
-        Выводит результаты тестов для каждого столбца. Если хотя бы один тест обнаруживает
-        нестационарность, выводит сообщение с указанием тестов и их p-value.
-        """
-        for column in columns:
-            if column in self.df.columns:
-                series = self.df[column].dropna()
+    #     Выводит результаты тестов для каждого столбца. Если хотя бы один тест обнаруживает
+    #     нестационарность, выводит сообщение с указанием тестов и их p-value.
+    #     """
+    #     for column in columns:
+    #         if column in self.df.columns:
+    #             series = self.df[column].dropna()
 
-                # Результаты тестов
-                pp_result = PhillipsPerron(series)
-                adf_result = adfuller(series, autolag='AIC')
+    #             # Результаты тестов
+    #             pp_result = PhillipsPerron(series)
+    #             adf_result = adfuller(series, autolag='AIC')
 
-                # Проверка p-value для каждого теста
-                non_stationary_tests = []
+    #             # Проверка p-value для каждого теста
+    #             non_stationary_tests = []
 
-                if pp_result.pvalue > 0.05:
-                    non_stationary_tests.append(f"Phillips-Perron (p-value: {pp_result.pvalue:.5f})")
+    #             if pp_result.pvalue > 0.05:
+    #                 non_stationary_tests.append(f"Phillips-Perron (p-value: {pp_result.pvalue:.5f})")
 
-                if adf_result[1] > 0.05:
-                    non_stationary_tests.append(f"ADF (p-value: {adf_result[1]:.5f})")
+    #             if adf_result[1] > 0.05:
+    #                 non_stationary_tests.append(f"ADF (p-value: {adf_result[1]:.5f})")
 
-                # Вывод результатов
-                if non_stationary_tests:
-                    print(f"Столбец: {column}")
-                    print("  Нестационарность обнаружена в следующих тестах:")
-                    for test in non_stationary_tests:
-                        print(f"    - {test}")
-            else:
-                print(f"Столбец '{column}' не найден в DataFrame.")
+    #             # Вывод результатов
+    #             if non_stationary_tests:
+    #                 print(f"Столбец: {column}")
+    #                 print("  Нестационарность обнаружена в следующих тестах:")
+    #                 for test in non_stationary_tests:
+    #                     print(f"    - {test}")
+    #         else:
+    #             print(f"Столбец '{column}' не найден в DataFrame.")
     
 
 
