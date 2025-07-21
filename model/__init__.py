d8f258f7ce4344d3c7d45c0e7d3398db24f65bfa
ellvalerie <117084699+ellvalerie@users.noreply.github.com>
2025-07-21 23:04:48 +0300

    galina

--- model/__init__.py
+++ model/__init__.py
@@ -2,7 +2,7 @@ from .data import FinData
 from .newsdata import NewsFinData
 from .preprocessing import train_valid_split, train_valid_test_split, merged_split, train_valid_split_candles, mul_PCA
 from .model import CatboostFinModel, SVMFinModel
-from .metrics import precision_long, recall_long, f1_metric_long
+from .metrics import precision_long, recall_long, f1_metric_long, f_beta_metric_long
 from .test import test_average_return
 from . import features, data, preprocessing, model
 from . import test
--- model/metrics.py
+++ model/metrics.py
@@ -1,23 +1,68 @@
-def true_positive(y_pred, close, commission):
+
+def true_positive_long(y_pred, close, commission):
     return ((y_pred == 1) & (close.shift(-1) / close > 1 + commission)).sum()
 
-def false_positive(y_pred, close, commission):
+def true_negative_long(y_pred, close, commission):
+    return ((y_pred == 0) & (close.shift(-1) / close <= 1 + commission)).sum()
+
+def false_positive_long(y_pred, close, commission):
     return ((y_pred == 1) & (close.shift(-1) / close <= 1 + commission)).sum()
 
-def false_negative(y_pred, close, commission):
+def false_negative_long(y_pred, close, commission):
     return ((y_pred == 0) & (close.shift(-1) / close > 1 + commission)).sum()
 
+def accuracy_long(y_pred, close, commission):
+    tp = true_positive_long(y_pred, close, commission)
+    fp = false_positive_long(y_pred, close, commission)
+    tn = true_negative_long(y_pred, close, commission)
+    fn = false_negative_long(y_pred, close, commission)
+    return (tp + fp) / (tp + fp + tn + fn)
+
 def precision_long(y_pred, close, commission):
-    tp = true_positive(y_pred, close, commission)
-    fp = false_positive(y_pred, close, commission)
+    tp = true_positive_long(y_pred, close, commission)
+    fp = false_positive_long(y_pred, close, commission)
     return tp / (tp + fp) if (tp + fp) > 0 else 0
 
 def recall_long(y_pred, close, commission):
-    tp = true_positive(y_pred, close, commission)
-    fn = false_negative(y_pred, close, commission)
+    tp = true_positive_long(y_pred, close, commission)
+    fn = false_negative_long(y_pred, close, commission)
     return tp / (tp + fn) if (tp + fn) > 0 else 0
 
-def f1_metric_long(y_pred, close, commission):
+def fbeta_metric_long(y_pred, close, commission, beta = 0.005):
     pr = precision_long(y_pred, close, commission)
     rec = recall_long(y_pred, close, commission)
-    return 2 * pr * rec / (pr + rec) if (pr + rec) > 0 else 0
+    return (1 + beta * beta) * pr * rec / (beta * beta * pr + rec) if (beta * beta * pr + rec) > 0 else 0
+
+def true_positive_short(y_pred, close, commission):
+    return ((y_pred == 1) & (close / close.shift(-1) < 1 + commission)).sum() # P = 1: close / close.shift(-1) < 1 + commission
+
+def true_negative_short(y_pred, close, commission):
+    return ((y_pred == 0) & (close / close.shift(-1) >= 1 + commission)).sum() # N = 0 : close / close.shift(-1) >= 1 + commission 
+
+def false_positive_short(y_pred, close, commission):
+    return ((y_pred == 1) & (close / close.shift(-1) >= 1 + commission)).sum()
+
+def false_negative_short(y_pred, close, commission):
+    return ((y_pred == 0) & (close / close.shift(-1) < 1 + commission)).sum()
+
+def accuracy_short(y_pred, close, commission):
+    tp = true_positive_short(y_pred, close, commission)
+    fp = false_positive_short(y_pred, close, commission)
+    tn = true_negative_short(y_pred, close, commission)
+    fn = false_negative_short(y_pred, close, commission)
+    return (tp + fp) / (tp + fp + tn + fn)
+
+def precision_short(y_pred, close, commission):
+    tn = true_negative_short(y_pred, close, commission)
+    fn = false_negative_short(y_pred, close, commission)
+    return tn / (tn + fn) if (tn + fn) > 0 else 0
+
+def recall_short(y_pred, close, commission):
+    tn = true_negative_short(y_pred, close, commission)
+    fp = false_positive_short(y_pred, close, commission)
+    return tn / (tn + fp) if (tn + fp) > 0 else 0
+
+def fbeta_metric_short(y_pred, close, commission, beta=0.005):
+    pr = precision_short(y_pred, close, commission)
+    rec = recall_short(y_pred, close, commission)
+    return (1 + beta * beta) * pr * rec / (beta * beta * pr + rec) if (beta * beta * pr + rec) > 0 else 0
