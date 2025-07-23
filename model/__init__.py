f7d3e6cc04c3892810449e9274395299c8c77754
ellvalerie <117084699+ellvalerie@users.noreply.github.com>
2025-07-24 01:03:14 +0300

    galina-3

--- model/__init__.py
+++ model/__init__.py
@@ -2,7 +2,7 @@ from .data import FinData
 from .newsdata import NewsFinData
 from .preprocessing import train_valid_split, train_valid_test_split, merged_split, train_valid_split_candles, mul_PCA
 from .model import CatboostFinModel, SVMFinModel
-from .metrics import precision_long, recall_long, fbeta_metric_long
+from .metrics import precision_long, recall_long, fbeta_metric_long, fbeta_metric_short, precision_short, recall_short
 from .test import test_average_return
 from . import features, data, preprocessing, model
 from . import test
