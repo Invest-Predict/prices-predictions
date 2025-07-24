37f2d60e93cdc8235d26c5f7da506d9346a36cb5
NastiaBur <s22d_burkova@179.ru>
2025-07-24 23:02:42 +0300

    SOOOOOOORRYYYYYYYYYYY

--- model/__init__.py
+++ model/__init__.py
@@ -2,11 +2,7 @@ from .data import FinData
 from .newsdata import NewsFinData
 from .preprocessing import train_valid_split, train_valid_test_split, merged_split, train_valid_split_candles, mul_PCA
 from .model import CatboostFinModel, SVMFinModel
-<<<<<<< HEAD
-from .metrics import precision_long, recall_long, f1_metric_long, precision_short, recall_short, f1_metric_short
-=======
 from .metrics import precision_long, recall_long, fbeta_metric_long, fbeta_metric_short, precision_short, recall_short
->>>>>>> f7d3e6cc04c3892810449e9274395299c8c77754
 from .test import test_average_return
 from . import features, data, preprocessing, model
 from . import test
